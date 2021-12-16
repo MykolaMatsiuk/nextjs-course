c2095ff14af8bc416f1c66415abea48e9311e58c
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-12-16 11:18:10 +0200

    pre-rendering & data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
+import path from 'path';
+import fs from 'fs/promises';
+import Link from 'next/link';
+
 function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+	return (
+		<ul>
+			{props.products.map((product) => (
+				<li key={product.id}>
+					<Link href={`/products/${product.id}`}>
+						{product.title}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+}
+
+export async function getStaticProps() {
+	console.log('(Re)Generating...');
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+	const jsonData = await fs.readFile(filePath);
+	const { products } = JSON.parse(jsonData);
+
+	return {
+		props: { products },
+		revalidate: 1
+	};
 }
 
 export default HomePage;
