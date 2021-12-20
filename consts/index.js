d7b93ad86c9cdde62c6c45e23fcd55fc5e87077d
Mykola Matsiuk <mykola.matsiuk@gmail.com>
2021-12-20 14:57:57 +0200

    initial

--- consts/index.js
+++
@@ -1,2 +0,0 @@
-export const URL =
-	'https://nextjs-course-cc530-default-rtdb.firebaseio.com/events.json';
--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,9 @@
-import Head from 'next/head';
-
-import EventList from '../components/events/event-list';
-import { getFeaturedEvents } from '../api-helpers';
-
-function HomePage({ featuredEvents }) {
+function HomePage() {
 	return (
 		<div>
-			<Head>
-				<title>NextJS Events</title>
-				<meta
-					name="description"
-					content="Find a lot of great events for your life"
-				/>
-			</Head>
-			<EventList items={featuredEvents} />
+			<h1>The Home Page</h1>
 		</div>
 	);
-}
-
-export async function getStaticProps() {
-	const featuredEvents = await getFeaturedEvents();
-
-	return {
-		props: { featuredEvents },
-		revalidate: 10
-	};
 }
 
 export default HomePage;
